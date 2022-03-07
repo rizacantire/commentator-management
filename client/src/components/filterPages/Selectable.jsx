0177c2d9913f483a062e788118a99d00853ac20e
rza35 <riza_tire@hotmail.com>
2022-03-07 17:13:03 +0300

    select

--- client/src/components/filterPages/Selectable.jsx
+++ client/src/components/filterPages/Selectable.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 import {commentatorList} from "../../redux/reduce/commentatorSlice";
 import { useSelector } from "react-redux";
 import {Select,MenuItem} from "@mui/material"
 
 export default function Selectable() {
   const getCommentators = useSelector(commentatorList);
+  const [value, setValue] = useState("");
+  const handleChange = event => setValue(event.target.value);
+  console.log(value);
   return (
     <div>
-      <Select size={"small"} >
+      <Select size={"small"} onChange={handleChange}>
       <MenuItem value="" > <em>None</em></MenuItem>
          {getCommentators.map((s) => (
-          <MenuItem value={s.id}>{s.firstName} {s.lastName}</MenuItem>
+          <MenuItem key={s.id} value={s.id}>{s.firstName} {s.lastName}</MenuItem>
         ))} 
       </Select>
     </div>
